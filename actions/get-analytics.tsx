d34dbe1e8ed0536f99bae35067c2cf45ba098e4c
yordanluturyalii <yordanluturyali@gmail.com>
2024-09-11 18:51:31 +0700

    feat: Teacher Analytics Dashboard

---
+++ actions/get-analytics.tsx
@@ -0,0 +1,54 @@
+import { db } from "@/lib/db";
+import { Courses, Purchases } from "@prisma/client"
+import { NextResponse } from "next/server";
+
+type PurchaseWithCourse = Purchases & {
+    course: Courses;
+};
+
+const groupByCourse = (purchases : PurchaseWithCourse[]) => {
+    const grouped: {[courseTitle: string]: number} = {};
+
+    purchases.forEach(purchase => {
+        const courseTitle = purchase.course.title;
+        if (!grouped[courseTitle]) grouped[courseTitle] = 0;
+
+        grouped[courseTitle] += purchase.course.price!;
+    });
+
+    return grouped;
+}
+
+export const getAnalytics = async(userId : string) => {
+    try {
+        const purchases = await db.purchases.findMany({
+            where: {
+                course: {
+                    userId: userId,
+                }
+            }, 
+            include: {
+                course: true
+            }
+        })
+        
+        const groupedEarnings = groupByCourse(purchases);
+
+        const data = Object.entries(groupedEarnings).map(([courseTitle, total]) => ({
+            name: courseTitle,
+            total: total
+        }));
+
+        const totalRevenue = data.reduce((acc, cur) => acc + cur.total, 0);
+        const totalSales = purchases.length;
+
+        return {
+            data, 
+            totalRevenue, 
+            totalSales
+        };
+    } catch (error) {
+        console.log("[GET_ANALYTICS]", error)
+        return new NextResponse("Internal Error", {status: 500});
+    }
+}
--- lib/format.ts
+++ lib/format.ts
@@ -1,5 +1,5 @@
 export const formatPrice = (price : number) => {
-    return new Intl.NumberFormat("en-USD", {
+    return new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD"
     }).format(price);
